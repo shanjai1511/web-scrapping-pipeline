908887733cb94bd52b73b203d20e1473f1cb8caf
Shanjai R <shanjai1511@gmail.com>
2024-07-19 21:57:21 +0530

    Wokring code for url collector #5
    Desccription:
    changed structure to oops
    made mode status recordings
    added cache

---
+++ url_collector/apple_aus/jbhifi_nz_com_apple_aus.py
@@ -0,0 +1,19 @@
+import pdb
+from common_module import CommonModule
+
+class JbhifiNzComAppleAus(CommonModule):
+    def get_final_url(self, url, depth, current_depth_level):
+        page_url = []
+        try:
+            #pdb.set_trace()
+            dom = self.get_page_content_hash(url)
+            if dom["status_code"] != 200:
+                raise Exception("No proper DOM found")
+            parsed_tree = self.get_parsed_tree(dom)
+            if parsed_tree is None:
+                raise Exception("Parsing failed")
+            #pdb.set_trace()
+            page_url = self.get_value_from_xpath(parsed_tree, "//div[@class='listing-prod-title']/a[@class='text-truncate']/@href", "all")
+        except Exception as e:
+            print(f"Exception occurred: {e}")
+        return page_url
