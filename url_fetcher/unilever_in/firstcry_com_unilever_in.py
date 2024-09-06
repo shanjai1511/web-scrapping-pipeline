2896e7caaadffec73fe5de7aba53c54164898cdb
Shanjai R <86219318+shanjai1511@users.noreply.github.com>
2024-09-06 13:49:09 +0000

    new pro

--- url_fetcher/unilever_in/firstcry_com_unilever_in.py
+++
@@ -1,5 +0,0 @@
-from sdf_module import *
-class FirstcryComUnileverIn:
-    def get_page_content(self, url, args_hash):
-        page_content = CommonModule.get_page_content_hash(url, args_hash)
-        return page_content
