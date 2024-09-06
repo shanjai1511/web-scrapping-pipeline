53916870869534cfa126f74eac9b4cf753a47b68
Shanjai R <shanjai1511@gmail.com>
2024-09-06 19:06:54 +0530

    new project

---
+++ url_collector/unilever_in/firstcry_com_unilever_in.py
@@ -0,0 +1,17 @@
+from sdf_module import CommonModule
+
+class FirstcryComUnileverIn(CommonModule):
+    def get_final_url(self, url, depth, current_depth_level):
+        page_url = []
+        try:
+            dom = self.get_page_content_hash(url)
+            if dom["status_code"] != 200:
+                raise Exception("No proper DOM found")
+            parsed_tree = self.get_parsed_tree(dom)
+            if parsed_tree is None:
+                raise Exception("Parsing failed")
+            page_url = self.get_value_from_xpath(parsed_tree, "xpath", "all/first")
+        except Exception as e:
+            print(f"Exception occurred: "+ e)
+        return page_url
+    
